2add7f896a217453bfc3429d70754181f7b5df64
‘Lekky <lekkyjay@yahoo.com>
2023-10-27 18:05:09 +0200

    Retrieved session from server component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import UserInfo from '@/components/UserInfo'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)  
+
   return (
     <div className="grid place-items-center h-screen -mt-24">
       <UserInfo />
+      <div>
+        <h1>GetServerSession Data</h1>
+        <p>Name: {session?.user?.name}</p>
+        <p>Email: {session?.user?.email}</p>
+      </div>
     </div>
   )
 }
